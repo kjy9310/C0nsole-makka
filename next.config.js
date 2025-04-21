e6062867640b03a42faa3d0d83d403f121c9e88a
makka <makka@DESKTOP-OLOE990.>
2025-04-21 23:02:05 +0900

    added workaround for mdx import source file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
-// next.config.js
+const path = require('path');
 const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/
+  extension: /\.mdx?$/,
 });
 
 module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx']
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  experimental: {
+    mdxRs: false
+  },
+  webpack: (config) => {
+    config.resolve.alias['next-mdx-import-source-file'] = path.resolve(__dirname, 'lib/next-mdx-import-source-file');
+    return config;
+  }
 });
