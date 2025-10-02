0fb44460cb269ed7c6d8b21f01a190a6aa8146d3
makka <aa019@hotmail.com>
2025-10-03 00:26:05 +0900

    gpt post added

--- data/blogList.js
+++ data/blogList.js
@@ -5,6 +5,11 @@ export const blogList = [
     "date": "2025-10-01T00:00:00.000Z"
   },
   {
+    "slug": "gpt-chat-blog",
+    "title": "C0nsole-makka: DOS 감성으로 쓰는 나만의 블로그",
+    "date": "2025-05-11T00:00:00.000Z"
+  },
+  {
     "slug": "hello",
     "title": "Hello World",
     "date": "2025-04-22T00:00:00.000Z"
